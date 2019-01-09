b56306243db7fee198bfa1d6c1dd267d991ff706
Sergiy Svitlookyi <sergiy.svitlookyi@itera.no>
2019-01-09 09:10:56 +0200

    Initial commit

---
+++ routes/admin.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/admin');
+
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
+
+router.get('/products', adminController.getProducts);
+
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
+
+exports.routes = router;
